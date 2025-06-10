da523cffa4aeb198b10c7aac5aab261f9a55339c
jonatan <extremeshow046@gmail.com>
2025-06-10 09:44:31 -0300

    Add files via upload

---
+++ models/Locacao.js
@@ -0,0 +1,27 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('.../config/database');
+
+const Localizacao = sequelize.define('locacoes',{
+    {
+    loc_id:{
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoincrement: true
+},
+loc_usu_id: {
+    typer:DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+        model: 'usuarios',
+        key:'usu_id'
+    }
+}
+},
+loc_sala_id: {
+    type: DataTypes.INTEGER,
+    allowNull:false,
+    references:{
+        model: 'salas',
+        key:'sal_id'
+    }
+},
